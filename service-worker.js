7e10258ed1205ede5a2c208e1c69195fe0854ab6
Daniel <66692561+MeDaniel785@users.noreply.github.com>
2022-11-11 13:20:57 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,12 @@
+if('serviceWorker' in navigator) {
+  let registration;
+
+  const registerServiceWorker = async () => {
+    registration = await          navigator.serviceWorker.register('./service-worker.js');
+  };
+
+  registerServiceWorker();
+}
 // give your cache a name
 const cacheName = 'web-store';
 
