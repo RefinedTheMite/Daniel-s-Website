fd63f813d4598638c70173ce3d06a020b408e802
Daniel <66692561+MeDaniel785@users.noreply.github.com>
2022-11-11 13:16:41 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,3 +7,19 @@ if('serviceWorker' in navigator) {
 
   registerServiceWorker();
 }
+const cacheName = 'my-cache';
+const filestoCache = [
+  '/',
+  '/favicon.ico',
+  '/index.html',
+  '/home.html',
+  '/css/styles.css',
+  '/js/app.js',
+  '/img/logo.png'
+]; 
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName)
+    .then(cache => cache.addAll(filesToCache))
+  );
+});
